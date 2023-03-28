3f6589e984f77319c053e6ac5f381a93f898b189
maziwei <201512895@qq.com>
2023-03-28 11:25:15 +0800

    init

---
+++ spring-cloud-alibaba-provider-8001/src/main/java/com/example/SpringCloudAlibabaProvider8001Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaProvider8001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAlibabaProvider8001Application.class, args);
+	}
+
+}
