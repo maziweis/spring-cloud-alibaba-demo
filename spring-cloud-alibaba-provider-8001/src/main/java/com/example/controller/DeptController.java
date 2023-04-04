43e3423e59bea29b5b3364727ae4f41f7a5c7036
maziwei <201512895@qq.com>
2023-04-04 10:54:26 +0800

    init

---
+++ spring-cloud-alibaba-provider-8001/src/main/java/com/example/controller/DeptController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/dept/nacos/")
+@Slf4j
+public class DeptController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "<h2>服务访问成功！</h2>服务名：spring-cloud-alibaba-provider<br /> 端口号： " + serverPort + "<br /> 传入的参数：" + id;
+    }
+}
